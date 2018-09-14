9ece630776d45d228d5cea753ce55f9ba8797c39
Steven Imus <imussg@gmail.com>
2018-09-14 19:54:45 -0400

    changed the about me

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -11,11 +11,14 @@ class Aboutme extends Component {
 						About Me
 					</h1>
 					<img className="about-me-pic"src={picture} />
-					I got my start in programming at age 11 continuing to dabble (though never professionally)
-					throughout my life.  After working with a friend of mine to create and manage a software
-					infrastructure for a IT hardware recycling facility (Akooba Inc.) for a year and a half,
-					I filled in my knowledge with Thinkful's online program and now am ready to solve problems
-					professionally as a software developer.
+					In 7th grade I took my first programming class: Visual Basic.  Throughout high school and 
+					college I would continue to take programming classes in a variety of topics and languages,
+					but would eventually graduate with a Philosophy degree from Miami University (Ohio) compelled
+					by the belief that my softer skills were underdeveloped and somewhat assuming that I had plenty
+					of time to find my way back to software development.  After working with a friend of mine to
+					help create and manage software for a IT hardware recycling facility (<a href="https://akooba.com/">Akooba Inc.</a>) for a year and a half,
+					I filled in my knowledge with a course in full-stack development with Thinkful while preparing myself for the job market
+					and am now ready to solve problems as a software developer
 			</div>
 		);
 	}
