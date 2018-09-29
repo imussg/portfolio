d66469a132e75f32a403a82104b4d19134e81df2
Steven Imus <imussg@gmail.com>
2018-09-29 17:24:30 -0400

    changed the about me

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -11,19 +11,11 @@ class Aboutme extends Component {
 						About Me
 					</h1>
 					<img className="about-me-pic"src={picture} alt="my-pic"/>
-					<p>In 7th grade I took my first programming class: Visual Basic.  Throughout high school and 
-					college I would continue to take programming classes in a variety of topics and languages,
-					but would eventually graduate with a Philosophy degree from Miami University (Ohio) compelled
-					by the belief that my softer skills were underdeveloped and somewhat assuming that I had plenty
-					of time to find my way back to software development.  After working with a friend of mine to
-					help create and manage software for a IT hardware recycling facility (<a href="https://akooba.com/">Akooba Inc.</a>) for a year and a half,
-					I filled in my knowledge with a course in full-stack development with Thinkful while preparing myself for the job market</p>
-					<p>I have always enjoyed problem solving through computer programming.  Solving logic puzzles and mathematics 
-					was always fun for me, and programming gave me the chance apply that creatively.  I look forward to working with a team of
-					programmers and solve some problems!
+					<p>I'm a full-stack developer looking forward to a career in software development and working with a team of great programmers!
+					I love building great software and solving hard problems
 					</p>
-					<p>In my free time I play video games, chess, tennis, and write code to ongoing projects.  Feel free to contact me at imussg@gmail.com
-					and check out <a href="https://github.com/imussg">my github</a> and/or my <a href="https://www.linkedin.com/in/steven-imus-3b3a1a52/">linkedin profile</a></p>
+					<p>Feel free to contact me at imussg@gmail.com and check out <a href="https://github.com/imussg">my github</a> and/or my 
+					<a href="https://www.linkedin.com/in/steven-imus-3b3a1a52/">linkedin profile</a></p>
 			</div>
 		);
 	}
