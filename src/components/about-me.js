7a8a04a2feb938157707ec2ce252bef54a8f6349
Steven Imus <imussg@gmail.com>
2018-09-29 17:39:28 -0400

    changed the about me again

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -11,8 +11,9 @@ class Aboutme extends Component {
 						About Me
 					</h1>
 					<img className="about-me-pic"src={picture} alt="my-pic"/>
-					<p>I'm a full-stack developer looking forward to a career in software development and working with a team of great programmers!
-					I love building great software and solving hard problems
+					<p>I'm a full-stack developer looking forward to a career in software development and working with a team of programmers.
+					After getting a philosophy degree from Miami university, I moved back to where I was born and raised, 
+					Washington D.C. where I continued to program up until now as I currently work at and manage a restaurant in downtown DC
 					</p>
 					<p>Feel free to contact me at imussg@gmail.com and check out <a href="https://github.com/imussg">my github</a> and/or my 
 					<a href="https://www.linkedin.com/in/steven-imus-3b3a1a52/">linkedin profile</a></p>
